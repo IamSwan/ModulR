9f613b6ffe2f235ce7df312ea0fdd0bcab402cf0
Swan Sarli <swan.sarli@epitech.eu>
2025-07-09 10:11:56 +0200

    refa: reworked project's architecture

---
+++ src/ModulR/init.lua
@@ -0,0 +1,17 @@
+--|| ModulR ||--
+
+--|| Main module ||--
+local ModulRCore = {}
+ModulRCore.__index = ModulRCore
+
+--|| Private Attributes ||--
+ModulRCore.Services = {}
+
+--|| Constructor ||--
+function ModulRCore.new()
+end
+
+--|| Public Methods ||--
+
+--|| Return ||--
+return ModulRCore
